b53aa6c5a76ec4ea9ad8f464f5303681c04d8795
Hangfei Lin <1023584582@qq.com>
2024-06-08 04:01:52 -0400

    commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { DisplayWeather } from "./componets/DisplayWeather";
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayWeather />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/DisplayWeather.tsx
@@ -0,0 +1,188 @@
+import React from "react";
+import { MainWrapper } from "./weather.module";
+import { AiOutlineSearch } from "react-icons/ai";
+import { WiHumidity } from "react-icons/wi";
+import { RiWindyFill } from "react-icons/ri";
+import {
+  BsFillSunFill,
+  BsCloudyFill,
+  BsFillCloudRainFill,
+  BsCloudFog2Fill,
+} from "react-icons/bs";
+import { RiLoaderFill } from "react-icons/ri";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import axios from "axios";
+
+interface WeatherDataProps {
+  name: string;
+
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  sys: {
+    country: string;
+  };
+  weather: {
+    main: string;
+  }[];
+  wind: {
+    speed: number;
+  };
+}
+export const DisplayWeather = () => {
+  const api_key = "3887422fc9f72e87a0a3178ab3b12462";
+  //   const api_key = "9df6e66dca7979f79240e83cb5bc6496";
+  //   const api_key = "0cc86d16bf572f78cdc96c096c7627e5";
+  const api_Endpoint = "https://api.openweathermap.org/data/2.5/";
+
+  const [weatherData, setWeatherData] = React.useState<WeatherDataProps | null>(
+    null
+  );
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const [searchCity, setSearchCity] = React.useState("");
+
+  const fetchCurrentWeather = React.useCallback(
+    async (lat: number, lon: number) => {
+      const url = `${api_Endpoint}weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
+      const response = await axios.get(url);
+      return response.data;
+    },
+    [api_Endpoint, api_key]
+  );
+
+  const fetchWeatherData = async (city: string) => {
+    try {
+      const url = `${api_Endpoint}weather?q=${city}&appid=${api_key}&units=metric`;
+      const searchResponse = await axios.get(url);
+
+      const currentWeatherData: WeatherDataProps = searchResponse.data;
+      return { currentWeatherData };
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const handleSearch = async () => {
+    if (searchCity.trim() === "") {
+      return;
+    }
+
+    try {
+      const { currentWeatherData } = await fetchWeatherData(searchCity);
+      setWeatherData(currentWeatherData);
+    } catch (error) {}
+  };
+
+  const iconChanger = (weather: string) => {
+    let iconElement: React.ReactNode;
+    let iconColor: string;
+
+    switch (weather) {
+      case "Rain":
+        iconElement = <BsFillCloudRainFill />;
+        iconColor = "#272829";
+        break;
+
+      case "Clear":
+        iconElement = <BsFillSunFill />;
+        iconColor = "#FFC436";
+        break;
+      case "Clouds":
+        iconElement = <BsCloudyFill />;
+        iconColor = "#102C57";
+        break;
+
+      case "Mist":
+        iconElement = <BsCloudFog2Fill />;
+        iconColor = "#279EFF";
+        break;
+      default:
+        iconElement = <TiWeatherPartlySunny />;
+        iconColor = "#7B2869";
+    }
+
+    return (
+      <span className="icon" style={{ color: iconColor }}>
+        {iconElement}
+      </span>
+    );
+  };
+
+  React.useEffect(() => {
+    // 定义一个异步函数fetchData，用来获取数据
+    const fetchData = async () => {
+      // 使用Geolocation API来获取用户的当前位置
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        // 从position对象中提取纬度和经度
+        const { latitude, longitude } = position.coords;
+        // 并行请求当前的天气数据
+        const [currentWeather] = await Promise.all([
+          fetchCurrentWeather(latitude, longitude),
+        ]);
+        // 将获取到的天气数据存储在状态变量中
+        setWeatherData(currentWeather);
+        // 将加载状态设置为true
+        setIsLoading(true);
+      });
+    };
+
+    fetchData();
+  }, [fetchCurrentWeather]);
+  return (
+    <MainWrapper>
+      <div className="container">
+        <div className="searchArea">
+          <input
+            type="text"
+            placeholder="enter a city"
+            value={searchCity}
+            onChange={(e) => setSearchCity(e.target.value)}
+          />
+
+          <div className="searchCircle">
+            <AiOutlineSearch className="searchIcon" onClick={handleSearch} />
+          </div>
+        </div>
+
+        {weatherData && isLoading ? (
+          <>
+            <div className="weatherArea">
+              <h1>{weatherData.name}</h1>
+              <span>{weatherData.sys.country}</span>
+              <div className="icon">
+                {iconChanger(weatherData.weather[0].main)}
+              </div>
+              <h1>{weatherData.main.temp.toFixed(0)}</h1>
+              <h2>{weatherData.weather[0].main}</h2>
+            </div>
+
+            <div className="bottomInfoArea">
+              <div className="humidityLevel">
+                <WiHumidity className="windIcon" />
+                <div className="humidInfo">
+                  <h1>{weatherData.main.humidity}%</h1>
+                  <p>Humidity</p>
+                </div>
+              </div>
+
+              <div className="wind">
+                <RiWindyFill className="windIcon" />
+                <div className="humidInfo">
+                  <h1>{weatherData.wind.speed}km/h</h1>
+                  <p>Wind speed</p>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="loading">
+            <RiLoaderFill className="loadingIcon" />
+            <p>Loading</p>
+          </div>
+        )}
+      </div>
+    </MainWrapper>
+  );
+};
